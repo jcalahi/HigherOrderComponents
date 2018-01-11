eea86374c753be0ad245e3f2f6fecac2b3646253
jcalahi <k0mikus@icloud.com>
2018-01-11 21:31:52 +0800

    Code completed

---
+++ src/components/require-auth-hoc.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+export default (ComposedComponent) => {
+  class Authentication extends Component {
+
+    static contextTypes = {
+      router: React.PropTypes.object
+    };
+
+    componentWillMount() {
+      if (!this.props.authenticated) {
+        this.context.router.push('/');
+      }
+    }
+
+    componentWillUpdate(nextProps) {
+      if (!nextProps.authenticated) {
+        this.context.router.push('/');
+      }
+    }
+
+    render() {
+      return (
+        <ComposedComponent { ...this.props } />
+      );
+    }
+  }
+
+  function mapStateToProps({ authenticated }) {
+    return { authenticated };
+  }
+
+  return connect(mapStateToProps)(Authentication);
+}
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { createStore, applyMiddleware } from 'redux';
 import { browserHistory, Router, Route } from 'react-router';
 import ReduxPromise from 'redux-promise';
 
+import requireAuth from './components/require-auth-hoc';
 import App from './components/app';
 import Resources from './components/resources';
 import reducers from './reducers';
@@ -15,7 +16,7 @@ ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <Router history={browserHistory}>
       <Route path="/" component={App}>
-        <Route path="resources" component={Resources} />
+        <Route path="resources" component={requireAuth(Resources)} />
       </Route>
     </Router>
   </Provider>
